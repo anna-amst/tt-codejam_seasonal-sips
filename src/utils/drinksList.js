072ceeae47d85bade3e816dd93a988619aa15625
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-27 15:07:35 -0600

    added src/utils/drinksList.js to export our drinkList array

---
+++ src/utils/drinksList.js
@@ -0,0 +1,35 @@
+const drinksList = [
+  {
+    name: "Bourbon and Peach Smash",
+    urls: [url("https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-vbLUVCUuOUg"),url("")],
+    tags: ["Bourbon","Peach", "Brown Sugar", "Ginger Beer", "Mint"],
+    recipe: url("https://stripedspatula.com/bourbon-peach-smash/"),
+    steps: {
+      step1: "",
+      step2: "",
+      step3: "",
+      step4: "",
+      step5: ""
+    }
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "",
+    images: [
+      url(""),
+      url(""),
+    ],
+    tags: [
+      "",
+      "",
+    ],
+    recipe: url(""),
+    steps: {
+      step1: "",
+      step2: "",
+      step3: "",
+      step4: "",
+      step5: ""
+    }
+  }
+]
