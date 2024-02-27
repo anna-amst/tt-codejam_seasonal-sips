77ba83105d1f8d89dd591d7aaaa7696df95a9f7c
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-27 14:28:42 -0600

    flesh out landing page to reflect our decisions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/drinksGallery.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="App-name">Seasonal Sips</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button type="submit" className="startButton">
+          Find Libation
+        </button>
       </header>
     </div>
   );
