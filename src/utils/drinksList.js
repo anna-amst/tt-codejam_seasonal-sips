0b79b4e564e5cb69f20d0517770d7b8465a594bc
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-27 15:09:01 -0600

    updated src/utils/drinksList.js

--- src/utils/drinksList.js
+++ src/utils/drinksList.js
@@ -1,8 +1,17 @@
 const drinksList = [
   {
     name: "Bourbon and Peach Smash",
-    urls: [url("https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-vbLUVCUuOUg"),url("")],
-    tags: ["Bourbon","Peach", "Brown Sugar", "Ginger Beer", "Mint"],
+    urls: [
+      url("https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-vbLUVCUuOUg"),
+      url("")
+    ],
+    tags: [
+      "Bourbon",
+      "Peach",
+      "Brown Sugar",
+      "Ginger Beer",
+      "Mint"
+    ],
     recipe: url("https://stripedspatula.com/bourbon-peach-smash/"),
     steps: {
       step1: "",
