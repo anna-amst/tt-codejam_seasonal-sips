31971c795b6f6e121fc4c75846a05eab7575c348
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-27 15:23:08 -0600

    updated src/utils/drinksList.js with recipe steps

--- src/utils/drinksList.js
+++ src/utils/drinksList.js
@@ -14,11 +14,11 @@ const drinksList = [
     ],
     recipe: url("https://stripedspatula.com/bourbon-peach-smash/"),
     steps: {
-      step1: "",
-      step2: "",
-      step3: "",
-      step4: "",
-      step5: ""
+      step1: "Add peaches, mint leaves, and simple syrup to the bottom of a cocktail shaker.",
+      step2: "Gently muddle to release the peach juices and mint oils. (Over-muddling the mint will cause it to become bitter)",
+      step3: "Add bourbon, fill the shaker with ice, and shake until well-chilled.",
+      step4: "Strain into a rocks glass filled with fresh ice and top with ginger beer or seltzer water.",
+      step5: "Garnish with fresh mint sprigs and a peach slice.",
     }
   },
   {
