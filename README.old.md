bd8f9143eac72ea33f0090830d1e1c992b01f1f7
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-27 13:21:12 -0600

    move to create-react-app module

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# tt-codejam_seasonal-sips
--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import logo from "./assets/tumbler.png";
-// import viteLogo from '/vite.svg'
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Seasonal Sips</h1>
-      <div className="card">
-        <img src={logo} alt="drink in a tumbler glass" />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
