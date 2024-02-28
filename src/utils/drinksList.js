3c6c3106ff0c5429f354a988e1c6cf03ac87f285
sociablerobot10 <19kam.sha@gmail.com>
2024-02-28 00:13:43 -0500

    add array of drinks

--- src/utils/drinksList.js
+++ src/utils/drinksList.js
@@ -2,43 +2,218 @@ export const drinksList = [
   {
     name: "Bourbon and Peach Smash",
     urls: [
-      url("https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-vbLUVCUuOUg"),
-      url("")
+      url(
+        "https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-vbLUVCUuOUg"
+      ),
+      url(""),
     ],
-    tags: [
-      "Bourbon",
-      "Peach",
-      "Brown Sugar",
-      "Ginger Beer",
-      "Mint"
-    ],
+    tags: ["Bourbon", "Peach", "Brown Sugar", "Ginger Beer", "Mint"],
     recipe: url("https://stripedspatula.com/bourbon-peach-smash/"),
     steps: {
-      step1: "Add peaches, mint leaves, and simple syrup to the bottom of a cocktail shaker.",
-      step2: "Gently muddle to release the peach juices and mint oils. (Over-muddling the mint will cause it to become bitter)",
-      step3: "Add bourbon, fill the shaker with ice, and shake until well-chilled.",
-      step4: "Strain into a rocks glass filled with fresh ice and top with ginger beer or seltzer water.",
+      step1:
+        "Add peaches, mint leaves, and simple syrup to the bottom of a cocktail shaker.",
+      step2:
+        "Gently muddle to release the peach juices and mint oils. (Over-muddling the mint will cause it to become bitter)",
+      step3:
+        "Add bourbon, fill the shaker with ice, and shake until well-chilled.",
+      step4:
+        "Strain into a rocks glass filled with fresh ice and top with ginger beer or seltzer water.",
       step5: "Garnish with fresh mint sprigs and a peach slice.",
-    }
+    },
   },
   {
     // blank object to make filling this file easier for everyone
-    name: "",
+    name: "Colletti Royale",
     images: [
-      url(""),
-      url(""),
+      url(
+        "https://unsplash.com/photos/a-couple-of-glasses-filled-with-drinks-on-top-of-a-table-kMNgSc97k4c"
+      ),
+    ],
+    tags: ["Blood Orange", "Lime", "Champagne", "Tequila", "Cointreau"],
+    recipe: url("https://www.liquor.com/recipes/colletti-royale/"),
+    steps: {
+      step1:
+        "Add the tequila, Cointreau, St-Germain, blood orange juice, lime juice and bitters into a shaker with ice and shake until well-chilled.",
+      step2: "Strain into a wine glass filled with fresh ice.",
+      step3: "Top with the rosé Champagne.",
+      step4: "Garnish with a blood orange wheel.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Gimlet",
+    images: [
+      url(
+        "https://unsplash.com/photos/clear-glass-cup-on-brown-wooden-surface-YNMjGIPgD_c"
+      ),
+    ],
+    tags: ["Gin", "Lime", "Honey Simple Syrup"],
+    recipe: url("https://www.gimmesomeoven.com/gimlet-recipe/"),
+    steps: {
+      step1:
+        "Combine all of the ingredients in a cocktail shaker or mason jar, along with a handful of ice, then cover and shake until chilled.",
+      step2:
+        "Strain into a chilled cocktail glass and serve with a slice of lime, if you would like.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Lemon Drop",
+    images: [
+      url(
+        "https://unsplash.com/photos/two-glasses-filled-with-yellow-liquid-sitting-on-a-table-dz-Ig7eOlys"
+      ),
+    ],
+    tags: ["Vodka", "Triple Sec", "Lemon Juice", "Simple Syrup"],
+    recipe: url("https://www.liquor.com/recipes/lemon-drop/"),
+    steps: {
+      step1:
+        "Coat the rim of a cocktail glass with sugar and set aside (do this a few minutes ahead so the sugar can dry and adhere well to the glass).",
+      step2:
+        "Add the vodka, triple sec, lemon juice and simple syrup to a shaker with ice and shake until well-chilled.",
+      step3: "Strain into the prepared glass.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Orange Blossom Whiskey Sour",
+    images: [
+      url(
+        "https://unsplash.com/photos/a-table-topped-with-wine-glasses-filled-with-liquid-LdkQf0HinPw"
+      ),
+    ],
+    tags: ["Bourbon", "Lemon Juice", "Orange Blossom", "Simple Syrup"],
+    recipe: url(
+      "https://www.trueloveandcoffee.com/home/orange-blossom-whiskey-sour-the-perfect-spring-cocktail"
+    ),
+    steps: {
+      step1:
+        "Combine bourbon, lemon juice and simple syrup in shaker with ice, shake until diluted and chilled (30 seconds).",
+      step2:
+        "Pour into a rocks glass with ice and garnish with the orange slice and cherry.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Blueberry Lime Mojito",
+    images: [
+      url(
+        "https://unsplash.com/photos/pink-liquids-served-in-drinking-glasses-Fk6ULMTxqj4"
+      ),
+    ],
+    tags: ["Lime Flavored Rum", "Blueberries", "Mint Leaves"],
+    recipe: url(
+      "https://www.bacardi.com/us/en/rum-cocktails/blueberry-lime-mojito/"
+    ),
+    steps: {
+      step1:
+        "Muddle blueberries in a highball glass then add the mint and gently press to release the oil.",
+      step2:
+        "Add crushed ice and BACARDÍ Lime Flavored Rum, and stir to incorporate.",
+      step3:
+        "Top with BACARDÍ Lime & Soda Real Rum Cocktail and more crushed ice.",
+      step4: "Garnish with a lime wheel and a mint sprig.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Raspberry Thyme Smash",
+    images: [
+      url(
+        "https://unsplash.com/photos/clear-drinking-glass-with-red-liquid-and-sliced-lemon-9Xvy-Y4bivg"
+      ),
+    ],
+    tags: ["Raspberries", "Gin", "Simple Syrup", "Lime Juice", "Thyme"],
+    recipe: url("https://www.bonappetit.com/recipe/raspberry-thyme-smash"),
+    steps: {
+      step1: "Remove leaves from 1 thyme sprig; place in cocktail shaker.",
+      step2:
+        "Add 6 berries; mash with muddler or wooden spoon. Fill cocktail shaker with ice; add gin, Simple Syrup, and lime juice.",
+      step3:
+        "Cover; shake vigorously until cold. Strain into highball glass filled with ice. Thread 1 raspberry onto remaining thyme sprig; place on rim of glass.",
+      step4:
+        "Stir 1 cup sugar and 1 cup water in small saucepan over low heat until sugar dissolves.",
+      step5: "Bring to boil; cool. Cover and chill.",
+    },
+  },
+  {
+    name: "Raspberry White Chocolate Latte",
+    images: [
+      url(
+        "https://unsplash.com/photos/person-pouring-brown-liquid-on-white-and-brown-ceramic-mug-EgDkl00Hiqw"
+      ),
+    ],
+    tags: ["Raspberry Syrup", "White Chocolate Sauce", "Coffee"],
+    recipe: url(
+      "https://jenaroundtheworld.com/raspberry-and-white-chocolate-iced-latte/"
+    ),
+    steps: {
+      step1: "Brew the espresso and set it aside.",
+      step2: "Add raspberry syrup and white chocolate sauce to the glass.",
+      step3: "Fill it with ice.",
+      step4:
+        "Add the espresso and then top with your milk choice. You can top it with whipped topping and fresh raspberries.",
+    },
+  },
+  {
+    // blank object to make filling this file easier for everyone
+    name: "Blackberry Bramble Cocktail",
+    images: [
+      url("https://unsplash.com/photos/a-glass-with-a-drink-in-it-tnr0D_P6ZAQ"),
+    ],
+    tags: ["Blackberries", "Lemon Juice", "Gin", "Blackberry Syrup"],
+    recipe: url("https://www.feastingathome.com/blackberry-bramble/"),
+    steps: {
+      step1: "Fill a glass with ice.",
+      step2:
+        "Muddle ripe juicy blackberries with fresh lemon juice in a shaker. Fill with ice.",
+      step3:
+        "Add gin, optional Blackberry liqueur, and sweetener of choice. Shake well.",
+      step4:
+        "Strain. Pour over ice and top with sparkling water, garnish with a twist of lemon and a fresh blackberry.",
+    },
+  },
+  {
+    name: "Raspberry White Chocolate Latte",
+    images: [
+      url(
+        "https://unsplash.com/photos/person-pouring-brown-liquid-on-white-and-brown-ceramic-mug-EgDkl00Hiqw"
+      ),
+    ],
+    tags: ["Raspberry Syrup", "White Chocolate Sauce", "Coffee"],
+    recipe: url(
+      "https://jenaroundtheworld.com/raspberry-and-white-chocolate-iced-latte/"
+    ),
+    steps: {
+      step1: "Brew the espresso and set it aside.",
+      step2: "Add raspberry syrup and white chocolate sauce to the glass.",
+      step3: "Fill it with ice.",
+      step4:
+        "Add the espresso and then top with your milk choice. You can top it with whipped topping and fresh raspberries.",
+    },
+  },
+  {
+    name: "Hugo Spritz",
+    images: [
+      url(
+        "https://unsplash.com/photos/clear-wine-glass-with-orange-liquid-8QPUalNTzbI"
+      ),
     ],
     tags: [
-      "",
-      "",
+      "St-Germain elderflower",
+      "Liqueur",
+      "Mint",
+      "Prosecco",
+      "Soda water",
     ],
-    recipe: url(""),
+    recipe: url("https://www.sipandfeast.com/hugo-spritz/"),
     steps: {
-      step1: "",
-      step2: "",
-      step3: "",
-      step4: "",
-      step5: ""
-    }
-  }
-]
+      step1: "Fill a wine glass with ice.",
+      step2:
+        "Place 6 mint leaves in the palm of your hand and lightly whack them with your other hand. Then, place the mint leaves in the glass.",
+      step3:
+        "Add 1 ounce of elderflower liqueur, 2 ounces of sparkling water or seltzer, and 3 slices of lime.",
+      step4: "Fill the glass to the top with prosecco and stir gently.",
+    },
+  },
+];
