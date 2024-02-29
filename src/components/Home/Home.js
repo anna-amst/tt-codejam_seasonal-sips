c582eb9b90e91d0b06f32e948957af20cfe010ad
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-29 10:56:12 -0600

    added new font and began blocking the drink carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,22 @@ const Home = () => {
     <div className="home">
       <div className="home__header">
         <p className="home__caption">Seasonal Sips</p>
-        <img src={logo} className="home__logo" alt="logo" />
+        <div className="home__container">
+          <div className="home__circle">
+            <img src={logo} className="home__logo" alt="logo" />
+          </div>
+          <div className="">
+            <p className="home__description">
+              Seasonal Sips offers a curated list of locally popular libations
+              and the places to find their ingredients or the places which
+              serve them for making your next night in or out with your friends into a
+              new and exciting adventure.</p>
+              <p className="home__description">
+              All while supporting local businesses you
+              might never have thought to visit before.
+            </p>
+          </div>
+        </div>
         <button type="button" id="start-button" className="home__submit">
           Find Libation
         </button>
