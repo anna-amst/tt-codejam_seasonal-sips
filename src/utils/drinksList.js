f5bc86aef3677ae8a24fc1787c5259f0b00fab09
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-02-29 15:54:08 -0600

    completed formatting url links to use for building a tag cloud from the array itself

--- src/utils/drinksList.js
+++ src/utils/drinksList.js
@@ -3,7 +3,7 @@ export const drinksList = [
     name: "Bourbon and Peach Smash",
     images: [
       'url("https://images.unsplash.com/photo-1691568769494-bd35c1e48917?q=80&w=1908&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
-      'url("https://images.unsplash.com/photo-1499638673689-79a0b5115d87?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")'
+      'url("https://images.unsplash.com/photo-1499638673689-79a0b5115d87?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Bourbon", "Peach", "Brown Sugar", "Ginger Beer", "Mint"],
     recipe: 'url("https://stripedspatula.com/bourbon-peach-smash/")',
@@ -22,10 +22,11 @@ export const drinksList = [
   {
     name: "Colletti Royale",
     images: [
-      url("https://images.unsplash.com/photo-1644247988716-e5b98d435f22?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"), url("https://images.unsplash.com/photo-1644320579405-7a1f22e52a55?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1644247988716-e5b98d435f22?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1644320579405-7a1f22e52a55?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Blood Orange", "Lime", "Champagne", "Tequila", "Cointreau"],
-    recipe: url("https://www.liquor.com/recipes/colletti-royale/"),
+    recipe: 'url("https://www.liquor.com/recipes/colletti-royale/")',
     steps: {
       step1:
         "Add the tequila, Cointreau, St-Germain, blood orange juice, lime juice and bitters into a shaker with ice and shake until well-chilled.",
@@ -37,11 +38,11 @@ export const drinksList = [
   {
     name: "Gimlet",
     images: [
-      url("https://images.unsplash.com/photo-1551538827-9c037cb4f32a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1623593688280-a5aec8ac4ae7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1551538827-9c037cb4f32a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1623593688280-a5aec8ac4ae7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Gin", "Lime", "Honey Simple Syrup"],
-    recipe: url("https://www.gimmesomeoven.com/gimlet-recipe/"),
+    recipe: 'url("https://www.gimmesomeoven.com/gimlet-recipe/")',
     steps: {
       step1:
         "Combine all of the ingredients in a cocktail shaker or mason jar, along with a handful of ice, then cover and shake until chilled.",
@@ -52,11 +53,11 @@ export const drinksList = [
   {
     name: "Lemon Drop",
     images: [
-      url("https://images.unsplash.com/photo-1639048759534-88ddc0824955?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1615887023516-9b6bcd559e87?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1639048759534-88ddc0824955?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1615887023516-9b6bcd559e87?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Vodka", "Triple Sec", "Lemon Juice", "Simple Syrup"],
-    recipe: url("https://www.liquor.com/recipes/lemon-drop/"),
+    recipe: 'url("https://www.liquor.com/recipes/lemon-drop/")',
     steps: {
       step1:
         "Coat the rim of a cocktail glass with sugar and set aside (do this a few minutes ahead so the sugar can dry and adhere well to the glass).",
@@ -68,15 +69,12 @@ export const drinksList = [
   {
     name: "Orange Blossom Whiskey Sour",
     images: [
-      url(
-        "https://plus.unsplash.com/premium_photo-1690056321886-e35b0f0268d5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      ),
-      url("https://images.unsplash.com/photo-1615887023544-3a566f29d822?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://plus.unsplash.com/premium_photo-1690056321886-e35b0f0268d5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1615887023544-3a566f29d822?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Bourbon", "Lemon Juice", "Orange Blossom", "Simple Syrup"],
-    recipe: url(
-      "https://www.trueloveandcoffee.com/home/orange-blossom-whiskey-sour-the-perfect-spring-cocktail"
-    ),
+    recipe:
+      'url("https://www.trueloveandcoffee.com/home/orange-blossom-whiskey-sour-the-perfect-spring-cocktail")',
     steps: {
       step1:
         "Combine bourbon, lemon juice and simple syrup in shaker with ice, shake until diluted and chilled (30 seconds).",
@@ -88,14 +86,12 @@ export const drinksList = [
     // blank object to make filling this file easier for everyone
     name: "Blueberry Lime Mojito",
     images: [
-      url(
-        "https://images.unsplash.com/photo-1504310578167-435ac09e69f3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      ), url("https://images.unsplash.com/photo-1605248152567-5af30fbe14d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1504310578167-435ac09e69f3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1605248152567-5af30fbe14d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Lime Flavored Rum", "Blueberries", "Mint Leaves"],
-    recipe: url(
-      "https://www.bacardi.com/us/en/rum-cocktails/blueberry-lime-mojito/"
-    ),
+    recipe:
+      'url("https://www.bacardi.com/us/en/rum-cocktails/blueberry-lime-mojito/")',
     steps: {
       step1:
         "Muddle blueberries in a highball glass then add the mint and gently press to release the oil.",
@@ -109,11 +105,11 @@ export const drinksList = [
   {
     name: "Raspberry Thyme Smash",
     images: [
-      url("https://images.unsplash.com/photo-1595981266686-0cf387d0a608?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1595981267035-7b04ca84a82d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1595981266686-0cf387d0a608?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1595981267035-7b04ca84a82d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Raspberries", "Gin", "Simple Syrup", "Lime Juice", "Thyme"],
-    recipe: url("https://www.bonappetit.com/recipe/raspberry-thyme-smash"),
+    recipe: 'url("https://www.bonappetit.com/recipe/raspberry-thyme-smash")',
     steps: {
       step1: "Remove leaves from 1 thyme sprig; place in cocktail shaker.",
       step2:
@@ -128,13 +124,12 @@ export const drinksList = [
   {
     name: "Raspberry White Chocolate Latte",
     images: [
-      url("https://images.unsplash.com/photo-1620360289473-bfafadc16c57?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1562447457-579fc34967fb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1620360289473-bfafadc16c57?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1562447457-579fc34967fb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Raspberry Syrup", "White Chocolate Sauce", "Coffee"],
-    recipe: url(
-      "https://jenaroundtheworld.com/raspberry-and-white-chocolate-iced-latte/"
-    ),
+    recipe:
+      'url("https://jenaroundtheworld.com/raspberry-and-white-chocolate-iced-latte/")',
     steps: {
       step1: "Brew the espresso and set it aside.",
       step2: "Add raspberry syrup and white chocolate sauce to the glass.",
@@ -146,10 +141,11 @@ export const drinksList = [
   {
     name: "Blackberry Bramble Cocktail",
     images: [
-      url("https://images.unsplash.com/photo-1669144457196-7a0f484333f8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"), ("https://images.unsplash.com/photo-1513816861509-09733a9f9383?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1669144457196-7a0f484333f8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1513816861509-09733a9f9383?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
     tags: ["Blackberries", "Lemon Juice", "Gin", "Blackberry Syrup"],
-    recipe: url("https://www.feastingathome.com/blackberry-bramble/"),
+    recipe: 'url("https://www.feastingathome.com/blackberry-bramble/")',
     steps: {
       step1: "Fill a glass with ice.",
       step2:
@@ -163,16 +159,11 @@ export const drinksList = [
   {
     name: "Hugo Spritz",
     images: [
-      url("https://images.unsplash.com/photo-1556881286-fc6915169721?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1556881286-fc6915169721?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+      'url("https://images.unsplash.com/photo-1556881286-fc6915169721?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1556881286-fc6915169721?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: [
-      "St-Germain Elderflower Liqueur",
-      "Mint",
-      "Prosecco",
-      "Soda Water",
-    ],
-    recipe: url("https://www.sipandfeast.com/hugo-spritz/"),
+    tags: ["St-Germain Elderflower Liqueur", "Mint", "Prosecco", "Soda Water"],
+    recipe: 'url("https://www.sipandfeast.com/hugo-spritz/")',
     steps: {
       step1: "Fill a wine glass with ice.",
       step2:
@@ -185,46 +176,45 @@ export const drinksList = [
   {
     name: "Cucumber Collins",
     images: [
-      url("https://images.unsplash.com/photo-1602298134483-f4f469306bf6?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1597075759290-5c29a23c8a16?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+      'url("https://images.unsplash.com/photo-1602298134483-f4f469306bf6?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1597075759290-5c29a23c8a16?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: [
-      "Gin", "Cucumber", "Lime", "Simple Syrup"
-    ],
-    recipe: url("https://www.thespruceeats.com/cucumber-collins-4778647"),
+    tags: ["Gin", "Cucumber", "Lime", "Simple Syrup"],
+    recipe: 'url("https://www.thespruceeats.com/cucumber-collins-4778647")',
     steps: {
-      step1: "In a cocktail shaker, add the cucumber, lime, and simple syrup. Muddle well to release all the fruit juices. Add the gin, fill the shaker with ice, and shake well.",
-      step2: "Strain into a tall glass filled with fresh ice and cucumber ribbons. Top with seltzer.",
+      step1:
+        "In a cocktail shaker, add the cucumber, lime, and simple syrup. Muddle well to release all the fruit juices. Add the gin, fill the shaker with ice, and shake well.",
+      step2:
+        "Strain into a tall glass filled with fresh ice and cucumber ribbons. Top with seltzer.",
       step3: "Garnish with a lime slice and mint sprig. Serve and enjoy.",
     },
   },
   {
     name: "Aperol Spritz",
     images: [
-      url("https://images.unsplash.com/photo-1578467759626-f94129ea94a4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1588685344608-514d42e02603?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+      'url("https://images.unsplash.com/photo-1578467759626-f94129ea94a4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1588685344608-514d42e02603?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: ["Aperol", "Prosecco", "Club Soda", "Orange"
-    ],
-    recipe: url("https://cookieandkate.com/classic-aperol-spritz-recipe/"),
+    tags: ["Aperol", "Prosecco", "Club Soda", "Orange"],
+    recipe: 'url("https://cookieandkate.com/classic-aperol-spritz-recipe/")',
     steps: {
-      step1: "You’ll simply fill a wine glass with ice, then add Aperol, Prosecco, club soda and a slice of orange.",
-    }
+      step1:
+        "You’ll simply fill a wine glass with ice, then add Aperol, Prosecco, club soda and a slice of orange.",
+    },
   },
   {
-    name:"Apricot Sour",
+    name: "Apricot Sour",
     images: [
-      url("https://images.unsplash.com/photo-1602061257507-e0150debda3b?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1602835124432-0ca2d2e66183?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+      'url("https://images.unsplash.com/photo-1602061257507-e0150debda3b?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1602835124432-0ca2d2e66183?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: [
-      "Apricot Brandy",
-      "Sour Mix",
-    ],
-    recipe: url("https://www.thespruceeats.com/apricot-sour-cocktail-recipe-759410"),
+    tags: ["Apricot Brandy", "Sour Mix"],
+    recipe:
+      'url("https://www.thespruceeats.com/apricot-sour-cocktail-recipe-759410")',
     steps: {
       step1: "Fill a glass with ice.",
-      step2: "Pour 2oz Apricot Brandy and 2oz Sour Mix into a cocktail shaker filled with ice.",
+      step2:
+        "Pour 2oz Apricot Brandy and 2oz Sour Mix into a cocktail shaker filled with ice.",
       step3: "Shake Well.",
       step4: "Strain into glass and serve with a cherry on top.",
     },
@@ -232,33 +222,33 @@ export const drinksList = [
   {
     name: "Butterfly Pea Lemonade",
     images: [
-      url("https://plus.unsplash.com/premium_photo-1687871816021-38c6e4475845?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1708455398647-9f79425512fa?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+      'url("https://plus.unsplash.com/premium_photo-1687871816021-38c6e4475845?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1708455398647-9f79425512fa?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: ["Butterfly Pea Flower Tea", "Lemonade", "Ice", "Lemons"
-    ],
-    recipe: url("https://www.ohhowcivilized.com/butterfly-pea-flower-lemonade/"),
+    tags: ["Butterfly Pea Flower Tea", "Lemonade", "Ice", "Lemons"],
+    recipe:
+      'url("https://www.ohhowcivilized.com/butterfly-pea-flower-lemonade/")',
     steps: {
       step1: "Put butterfly pea flower tea bag and hot water into a teapot.",
-      step2: "Cover and steep. The water will turn a deep blue. Remove the tea bag and let cool.",
+      step2:
+        "Cover and steep. The water will turn a deep blue. Remove the tea bag and let cool.",
       step3: "Pour lemonade into a cup with ice.",
-      step4: "Add cooled down tea & serve. Garnish with thinly sliced lemon."
+      step4: "Add cooled down tea & serve. Garnish with thinly sliced lemon.",
     },
   },
   {
-    name:"Indigo Thrill",
+    name: "Indigo Thrill",
     images: [
-      url("https://images.unsplash.com/photo-1582269438702-578efa319292?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-      url("https://images.unsplash.com/photo-1582269438732-fe4d1452df3c?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+      'url("https://images.unsplash.com/photo-1582269438702-578efa319292?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      'url("https://images.unsplash.com/photo-1582269438732-fe4d1452df3c?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     ],
-    tags: ["Gin", "Vermouth", "Salers Aperitif", "Lavender Bitters"
-
-    ],
-    recipe: url("https://vinepair.com/cocktail-recipe/the-indigo-thrill-recipe/"),
+    tags: ["Gin", "Vermouth", "Salers Aperitif", "Lavender Bitters"],
+    recipe:
+      'url("https://vinepair.com/cocktail-recipe/the-indigo-thrill-recipe/")',
     steps: {
       step1: "Combine all ingredients in a cocktail shaker with ice, and stir.",
       step2: "Serve in a rocks glass over a large cube.",
-      step3: "Garnish with lemon zest and flower."
-    }
-  }
+      step3: "Garnish with lemon zest and flower.",
+    },
+  },
 ];
